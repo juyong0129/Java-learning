01c956137bcee5b93a89be22726bc4754d6f8092
Juyong <developerjuyong0129@gmail.com>
2025-10-25 18:38:59 +0900

    second

---
+++ src/Second/Variable.java
@@ -0,0 +1,33 @@
+package Second;
+
+public class Variable {
+    public static void main(String[] args) {
+        char ch1 = 'A';
+        char ch2 = 65;
+        char ch3 = '\uD55C'; //use unicode
+        int a = 65;
+        int b = -65;
+        float pi = 3.14F;
+        double pi2 = 3.14;
+        boolean bool = true;
+        var hello = "hello";
+        final int MAX = 100;
+        byte test = 10;
+        int itest = test; //묵시적 형 변환
+
+        System.out.println(ch1);
+        System.out.println(ch2);
+        System.out.println(ch3);
+        System.out.println(a);
+        System.out.println((char)a); //명시적 형 변환
+        System.out.println(b);
+        System.out.println((char)b);
+        System.out.println(pi);
+        System.out.println(pi2);
+        System.out.println(bool);
+        System.out.println(hello);
+        System.out.println(MAX);
+        System.out.println(test);
+        System.out.println(itest);
+    }
+}
